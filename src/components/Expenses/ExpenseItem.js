c7af2091081f8df1276314ec891dbab7d74a0237
Grady Ku <32969781+gkupdx@users.noreply.github.com>
2023-07-19 20:02:49 -0700

    Add new content

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,27 @@
 // ExpenseItem component - responsible for outputting the individual "expense" data
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
+import { useState } from "react";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2023, 7, 18);
-  // const expenseTitle = "Car Insurance";
-  // const expenseAmount = 294.67;
+  // let title = props.title;
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler} type="button">
+        Change Title
+      </button>
     </Card>
   );
 }
